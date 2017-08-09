05f4832686f422e005e517acfaf18a0e1b6e4516
Nate Rickard <nate.rickard@microsoft.com>
2017-08-09 11:18:57 -0400

    added keys file

---
+++ Sample/Xamarin.Cognitive.BingSpeech.Sample/Keys.cs
@@ -0,0 +1,12 @@
+﻿namespace Xamarin.Cognitive.BingSpeech.Sample
+{
+	public static class Keys
+	{
+		public static class BingSpeech
+		{
+			public const string BadSubscriptionKey = "GET ONE";
+
+			public const string SubscriptionKey = BadSubscriptionKey;
+		}
+	}
+}
