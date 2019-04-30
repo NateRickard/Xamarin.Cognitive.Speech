00724fbc19922bcd089427db5f11b8ceac95163c
Andrew Glatts <aglatts@customhouseinc.com>
2019-04-30 15:59:02 -0400

    Created SpeechRegion enum based on MS docs Added Speech Region Picker to MainPage.xaml Handle speech region changes in MainPage.xaml.cs Added SpeechRegion to AuthenticationClient Added SpeechRegion to BingSpeechApiClient.cs updated Endpoints.cs to reflect the new speech service uri
    Fixed bug in BingSpeechApiClient where local variables were not being passed into AuthenticationClient CTOR
    Removed older NuGet package references in order to update /test changes

--- Sample/Xamarin.Cognitive.BingSpeech.Sample/Keys.cs
+++ Sample/Xamarin.Cognitive.BingSpeech.Sample/Keys.cs
@@ -6,7 +6,7 @@ namespace Xamarin.Cognitive.BingSpeech.Sample
 		{
 			public const string BadSubscriptionKey = "!BAD KEY!"; // get a key here: https://azure.microsoft.com/en-us/try/cognitive-services/my-apis/?apiSlug=speech-api
 
-			public const string SubscriptionKey = "891d7678fde14d19b0a57e5a63a82404"; // replace with your own key here, e.g. "8a29574dc98a4a29566fa7f6440d3d39";
+			public const string SubscriptionKey = "BadSubscriptionKey"; // replace with your own key here, e.g. "8a29574dc98a4a29566fa7f6440d3d39";
 		}
 	}
 }
