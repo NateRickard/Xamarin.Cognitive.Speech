d183dcc8a0e005db6e9a4e1ac53790e38a6ef667
Nate Rickard <NateRickard@gmail.com>
2019-05-14 16:10:18 -0400

    added ability to set Query in endpoint

--- Xamarin.Cognitive.Speech/Model/Endpoint.cs
+++ Xamarin.Cognitive.Speech/Model/Endpoint.cs
@@ -8,24 +8,29 @@ namespace Xamarin.Cognitive.Speech
 	public class Endpoint
 	{
 		/// <summary>
-		/// Gets the protocol.
+		/// Gets the protocol of the endpoint.
 		/// </summary>
 		public string Protocol { get; private set; } = "https";
 
 		/// <summary>
-		/// Gets the host.
+		/// Gets the host of the endpoint.
 		/// </summary>
 		public string Host { get; private set; }
 
 		/// <summary>
-		/// Gets the port.
+		/// Gets the port of the endpoint.
 		/// </summary>
 		public int Port { get; private set; } = 443;
 
 		/// <summary>
-		/// Gets the path.
+		/// Gets the path portion of the endpoint.
 		/// </summary>
 		public string Path { get; private set; }
+
+		/// <summary>
+		/// Gets the Query portion of the endpoint.
+		/// </summary>
+		public string Query { get; private set; }
 
 		/// <summary>
 		/// Gets a value indicating if this endpoint should use the speech region as a prefix to the endpoint
@@ -40,13 +45,15 @@ namespace Xamarin.Cognitive.Speech
 		/// <param name="host">Host.</param>
 		/// <param name="port">Port.</param>
 		/// <param name="path">Path, including the leading '/'</param>
-		/// <param name="prefixWithRegion">If set to True, the speech region will prefix this endpoint, e.g. [Protocol]://centralus.[Host]:[Port]/[Path]</param>
-		public Endpoint (string protocol, string host, int port, string path, bool prefixWithRegion = true)
+		/// <param name="query">Query string appended to the URI. Will be appended after a ? in the URI if a value is present.</param>
+		/// <param name="prefixWithRegion">If set to True, the speech region will prefix this endpoint, e.g. [Protocol]://centralus.[Host]:[Port]/[Path]. Defaults to <c>true</c>.</param>
+		public Endpoint (string protocol, string host, int port, string path, string query = null, bool prefixWithRegion = true)
 		{
 			Protocol = protocol;
 			Host = host;
 			Port = port;
 			Path = path;
+			Query = query;
 			PrefixWithRegion = prefixWithRegion;
 		}
 
@@ -57,12 +64,14 @@ namespace Xamarin.Cognitive.Speech
 		/// <param name="host">Host.</param>
 		/// <param name="port">Port.</param>
 		/// <param name="path">Path, including the leading '/'</param>
-		/// <param name="prefixWithRegion">If set to True, the speech region will prefix this endpoint, e.g. [Protocol]://centralus.[Host]:[Port]/[Path]</param>
-		public Endpoint (string host, int port, string path, bool prefixWithRegion = true)
+		/// <param name="query">Query string appended to the URI. Will be appended after a ? in the URI if a value is present.</param>
+		/// <param name="prefixWithRegion">If set to True, the speech region will prefix this endpoint, e.g. [Protocol]://centralus.[Host]:[Port]/[Path]. Defaults to <c>true</c>.</param>
+		public Endpoint (string host, int port, string path, string query = null, bool prefixWithRegion = true)
 		{
 			Host = host;
 			Port = port;
 			Path = path;
+			Query = query;
 			PrefixWithRegion = prefixWithRegion;
 		}
 
@@ -72,11 +81,13 @@ namespace Xamarin.Cognitive.Speech
 		/// </summary>
 		/// <param name="host">Host.</param>
 		/// <param name="path">Path, including the leading '/'</param>
+		/// <param name="query">Query string appended to the URI. Will be appended after a ? in the URI if a value is present.</param>
 		/// <param name="prefixWithRegion">If set to True, the speech region will prefix this endpoint, e.g. [Protocol]://centralus.[Host]:[Port]/[Path]</param>
-		public Endpoint (string host, string path, bool prefixWithRegion = true)
+		public Endpoint (string host, string path, string query = null, bool prefixWithRegion = true)
 		{
 			Host = host;
 			Path = path;
+			Query = query;
 			PrefixWithRegion = prefixWithRegion;
 		}
 
@@ -86,7 +97,8 @@ namespace Xamarin.Cognitive.Speech
 			{
 				Scheme = this.Protocol,
 				Port = this.Port,
-				Path = this.Path
+				Path = this.Path,
+				Query = this.Query
 			};
 
 			if (PrefixWithRegion)
